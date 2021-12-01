c96972551f68a49989d71574f4b9d63a2dac23ba
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-01 22:28:53 +0100

    Update queries

--- queries.sql
+++ queries.sql
@@ -22,3 +22,22 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Write queries to answer the following questions:
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN DATE('1990-01-01') AND DATE('2000-12-31') GROUP BY species;

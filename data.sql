c5ad7804b4efe1b020eb54e90f946701ec6a4c5f
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-01 20:40:21 +0100

    Inserted more data to Table

--- data.sql
+++ data.sql
@@ -35,3 +35,73 @@ VALUES (
     TRUE,
     11
 );
+
+-- Insert more data here.
+
+
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+)
+VALUES (
+    'Charmander',
+    DATE '2020-02-08',
+    0,
+    FALSE,
+    -11
+),
+(
+    'Plantmon',
+    DATE '2022-11-15',
+    2,
+    TRUE,
+    -5.7
+),
+(
+    'Squirtle',
+    DATE '1993-04-02',
+    3,
+    FALSE,
+    -12.13
+),
+(
+    'Angemon',
+    DATE '2005-06-12',
+    1,
+    TRUE,
+    -45
+),
+(
+    'Boarmon',
+    DATE '2005-06-7',
+    7,
+    TRUE,
+    20.4
+),
+(
+    'Blossom',
+    DATE '1998-10-13',
+    3,
+    TRUE,
+    17
+);
+
+
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+-- Commit the transaction.
+-- Verify that change was made and persists after commit.
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+-- After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)
+-- Inside a transaction:
+-- Delete all animals born after Jan 1st, 2022.
+-- Create a savepoint for the transaction.
+-- Update all animals' weight to be their weight multiplied by -1.
+-- Rollback to the savepoint
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+-- Commit transaction

e1b14cd2bac95b4e8748607e2acd6c19eab823cd
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-01 20:57:16 +0100

    Updated the animal Tables inside a transaction

--- data.sql
+++ data.sql
@@ -89,9 +89,16 @@ VALUES (
     17
 );
 
-
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before tranasction.
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ SELECT * FROM animals;
+ ROllBACK;
+    SELECT * FROM animals;
 -- Inside a transaction:
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+
 
 -- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
 -- Commit the transaction.
